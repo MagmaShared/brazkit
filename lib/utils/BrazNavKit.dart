0f1cc2e6cd141d69c070d074f315219ea1f5a711
Emanuel Braz <fastencoding@gmail.com>
2019-09-24 21:42:40 -0300

    navkit via rota nomeada e sem necessidade de context

--- lib/utils/BrazNavKit.dart
+++ lib/utils/BrazNavKit.dart
@@ -2,6 +2,14 @@ import 'package:flutter/material.dart';
 
 class BrazNavKit {
 
+  GlobalKey<NavigatorState> navigatorKey = new GlobalKey<NavigatorState>();
+
+  static final BrazNavKit _instance = BrazNavKit._internal();
+  factory BrazNavKit() { 
+    return _instance; 
+  }
+  BrazNavKit._internal();
+
   static dynamic push(BuildContext context, Widget page) async {
     return await Navigator.push(context, MaterialPageRoute(builder : (context) => page ));
   }
@@ -13,4 +21,43 @@ class BrazNavKit {
   static void pop(BuildContext context){
     Navigator.pop(context);
   }
+
+  Future<dynamic> route(String routeName) {
+    return navigatorKey.currentState.pushNamed(routeName);
+  }
+
+  Future<dynamic> routeWithParams(String routeName, {dynamic arguments}) {
+    return navigatorKey.currentState.pushNamed(routeName, arguments: arguments);
+  }
+
+  bool backRoute() {
+    return navigatorKey.currentState.pop();
+  }
 }
+
+// Use:
+// in main.dart
+//return MaterialApp(      
+// navigatorKey: BrazNavKit().navigatorKey,
+// onGenerateRoute: router.generateRoute,
+// initialRoute: routes.LoginRoute,
+
+//router
+// Route<dynamic> generateRoute(RouteSettings settings) {
+//   switch (settings.name) {
+//     case routes.LoginRoute:
+//       return MaterialPageRoute(builder: (context) => LoginView());
+//     case routes.HomeRoute:
+//       var userName = settings.arguments as String;
+//       return MaterialPageRoute(
+//           builder: (context) => HomeView(userName: userName));
+//     default:
+//       return MaterialPageRoute(
+//         builder: (context) => Scaffold(
+//           body: Center(
+//             child: Text('No path for ${settings.name}'),
+//           ),
+//         ),
+//       );
+//   }
+// }
