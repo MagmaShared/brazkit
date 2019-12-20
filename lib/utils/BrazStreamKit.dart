40bbe66059831dbe61c6fef3111ab43c9f1840f1
Emanuel Braz <fastencoding@gmail.com>
2019-12-20 19:46:05 -0300

    validação adicional em BrazStreamKit

--- lib/utils/BrazStreamKit.dart
+++ lib/utils/BrazStreamKit.dart
@@ -5,7 +5,7 @@ import 'package:flutter/material.dart';
 class BrazStreamKit {
 
   static Timer runCancelableTimer(Timer oldTimer, VoidCallback callback, {Duration duration = const Duration(seconds: 5)}) {
-    oldTimer?.cancel();
+    if (oldTimer?.isActive ?? false) oldTimer?.cancel();
     oldTimer = null;
     return Timer(duration, callback);
   }
