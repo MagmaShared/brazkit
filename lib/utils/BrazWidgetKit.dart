60d46f682417e8d04bd006bc5ae791ee837824df
Emanuel Braz <fastencoding@gmail.com>
2020-03-11 17:30:27 -0300

    - Mudanca na forma como remove foco do widget (melhor performance) - Recuperar tamanho do appBar

--- lib/utils/BrazWidgetKit.dart
+++ lib/utils/BrazWidgetKit.dart
@@ -1,6 +1,20 @@
 import 'package:flutter/material.dart';
+
 class BrazWidgetKit {
   static void dismissFocus(BuildContext context) {
-    FocusScope.of(context).requestFocus(new FocusNode());
+    // FocusScope.of(context).requestFocus(new FocusNode());
+    FocusScopeNode currentFocus = FocusScope.of(context);
+    if (!currentFocus.hasPrimaryFocus) {
+      currentFocus.unfocus();
+    }
+  }
+
+  static double getAppBarHeight(BuildContext context){
+    final double statusbarHeight = MediaQuery
+        .of(context)
+        .padding
+        .top;
+
+    return statusbarHeight + kToolbarHeight;
   }
 }
